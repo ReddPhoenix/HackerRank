2e3d913bbd47bf3a0af57e2ed75e74ded9dd2135
Walter Guerrero <wsguerrero@gmail.com>
2021-05-30 23:48:21 -0600

    initial commit

---
+++ SQL/RevisingTheSelectQuery.sql
@@ -0,0 +1,29 @@
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+-- The CITY table is described as follows:
+
+-- FIELD        TYPE
+-- ID           NUMBER
+-- NAME         VARCHAR2(17)
+-- COUNTRYCODE  VARCHAR2(3)
+-- DISTRICT     VARCHAR2(20)
+-- POPULATION   NUMBER
+
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+
+
+Your Output (stdout)
+3878 Scottsdale USA Arizona 202705
+3965 Corona USA California 124966
+3973 Concord USA California 121780
+3977 Cedar Rapids USA Iowa 120758
+3982 Coral Springs USA Florida 117549
+
+
+Expected Output
+
+3878 Scottsdale USA Arizona 202705 
+3965 Corona USA California 124966 
+3973 Concord USA California 121780 
+3977 Cedar Rapids USA Iowa 120758 
+3982 Coral Springs USA Florida 117549 
