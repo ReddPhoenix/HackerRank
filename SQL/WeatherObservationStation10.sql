61b19e6094a74cd67facacda51bb87dfc4b160a0
Walter Guerrero <wsguerrero@gmail.com>
2021-06-01 22:57:56 -0600

    initial commit

---
+++ SQL/WeatherObservationStation10.sql
@@ -0,0 +1,161 @@
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+-- Input Format
+
+-- The STATION table is described as follows:
+
+
+-- FIELD        TYPE
+-- ID           NUMBER
+-- CITY         VARCHAR2(21)
+-- STATE        VARCHAR2(2)
+-- LAT_N        NUMBER
+-- LAT_W        NUMBER
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+SELECT DISTINCT CITY FROM STATION WHERE (CITY NOT LIKE '%a' 
+    AND CITY NOT LIKE '%e' 
+    AND CITY NOT LIKE '%i' 
+    AND CITY NOT LIKE '%o' 
+    AND CITY NOT LIKE '%u'); 
+
+
+-- Your Output (stdout) / Expected Outcome
+-- Kissee Mills
+-- Loma Mar
+-- Sandy Hook
+-- Tipton
+-- Arlington
+-- Turner
+-- Slidell
+-- Negreet
+-- Chignik Lagoon
+-- Hanna City
+-- Albany
+-- Monument
+-- Manchester
+-- Prescott
+-- Graettinger
+-- Sturgis
+-- Highwood
+-- Bowdon
+-- Tyler
+-- Watkins
+-- Republic
+-- Bowdon Junction
+-- Hoskinston
+-- Talbert
+-- Mccomb
+-- Kirk
+-- Carlock
+-- Seward
+-- Roy
+-- Pattonsburg
+-- Centertown
+-- Norvell
+-- Beaver Island
+-- Odin
+-- Jemison
+-- West Hills
+-- Culdesac
+-- Roselawn
+-- Forest Lakes
+-- San Simeon
+-- Little Rock
+-- Portland
+-- New Century
+-- Hampden
+-- Pine City
+-- Sandborn
+-- Seaton
+-- Prince Frederick
+-- Pomona Park
+-- Yazoo City
+-- Jolon
+-- Childs
+-- Shreveport
+-- Forest
+-- Sizerock
+-- Buffalo Creek
+-- Algonac
+-- Onaway
+-- Irvington
+-- Winsted
+-- Woodbury
+-- Hackleburg
+-- Soldier
+-- Arrowsmith
+-- Columbus
+-- Kirkland
+-- Wilton
+-- Busby
+-- Reeds
+-- Hayfork
+-- Mcbrides
+-- Lee Center
+-- Henderson
+-- Udall
+-- Palm Desert
+-- Benedict
+-- Oakfield
+-- Tamms
+-- Haubstadt
+-- Clancy
+-- Scotts Valley
+-- Norwood
+-- Elkton
+-- Bridgeport
+-- Cherry
+-- Griffin
+-- Pine Bluff
+-- Baldwin
+-- Pony
+-- Franklin
+-- Vulcan
+-- Prairie Du Rocher
+-- Alanson
+-- Carver
+-- Paron
+-- Winchester
+-- Greenview
+-- Lucerne Valley
+-- Cromwell
+-- Quinter
+-- Whitewater
+-- Round Pond
+-- Rockton
+-- Auburn
+-- North Berwick
+-- Richland
+-- Fremont
+-- Philipsburg
+-- Kensett
+-- Koleen
+-- Winslow
+-- Reasnor
+-- Frankfort Heights
+-- Linthicum Heights
+-- Pengilly
+-- Newton Center
+-- Newbury
+-- Kismet
+-- Canton
+-- Clipper Mills
+-- Pierre Part
+-- Bison
+-- Ridgway
+-- South Britain
+-- Rydal
+-- Deerfield
+-- Montreal
+-- Knob Lick
+-- Cranks
+-- Rives Junction
+-- Ledyard
+-- Norway
+-- Eros
+-- Rantoul
+-- Richmond Hill
+-- Fredericktown
+-- Glen Carbon
