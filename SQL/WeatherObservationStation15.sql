1d8cda9dee043f498eaab232f06e5e64097a8072
Walter Guerrero <wsguerrero@gmail.com>
2021-06-02 20:39:30 -0600

    initial commit

---
+++ SQL/WeatherObservationStation15.sql
@@ -0,0 +1,22 @@
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+
+-- Input Format
+
+-- The STATION table is described as follows:
+
+-- FIELD        TYPE
+-- ID           NUMBER
+-- CITY         VARCHAR2(21)
+-- STATE        VARCHAR2(2)
+-- LAT_N        NUMBER
+-- LAT_W        NUMBER
+
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
+SELECT ROUND(LONG_W, 4) FROM STATION WHERE LAT_N < 137.2345 ORDER BY LAT_N DESC LIMIT 1;
+
+
+-- Your Output (stdout) / Expected Outcome
+-- 117.2465
