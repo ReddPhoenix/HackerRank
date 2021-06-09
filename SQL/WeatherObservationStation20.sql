ccf4454d3a1263ccc6e522177ec7767157043f31
Walter Guerrero <wsguerrero@gmail.com>
2021-06-09 14:38:48 -0600

    solved

--- SQL/WeatherObservationStation20.sql
+++ SQL/WeatherObservationStation20.sql
@@ -13,3 +13,15 @@
 
 
 -- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
+SET @N := 0;
+
+SELECT COUNT(*) FROM STATION INTO @TOTAL;
+
+SELECT ROUND(AVG(A.LAT_N), 4) FROM (SELECT @N := @N +1 AS ROW_ID, LAT_N FROM STATION ORDER BY LAT_N) A WHERE CASE WHEN MOD(@TOTAL, 2) = 0 THEN A.ROW_ID IN (@TOTAL/2, (@TOTAL/2+1)) ELSE A.ROW_ID = (@TOTAL+1)/2 END
+
+
+-- Expected Output
+
+-- 83.8913
