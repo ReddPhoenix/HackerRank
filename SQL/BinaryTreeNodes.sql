1f96aba07097d7aa45a4e705ad0452bf990c9aea
Walter Guerrero <wsguerrero@gmail.com>
2021-06-11 17:30:55 -0600

    solved

--- SQL/BinaryTreeNodes.sql
+++ SQL/BinaryTreeNodes.sql
@@ -46,3 +46,28 @@
 --      (2)          (8)
 --     /   \        /   \
 --  (1)    (3)    (6)   (9)
+
+
+-- 'Root' no parent (null)
+-- If N is in P then 'Inner'
+-- Remaining N then 'Leaf'
+
+SELECT N, IF(P IS NULL, 'Root', IF(BST.N IN (SELECT P FROM BST), 'Inner', 'Leaf')) FROM BST ORDER BY N;
+
+
+-- Your Output (stdout) / Expected Output
+-- 1 Leaf
+-- 2 Inner
+-- 3 Leaf
+-- 4 Inner
+-- 5 Leaf
+-- 6 Inner
+-- 7 Leaf
+-- 8 Leaf
+-- 9 Inner
+-- 10 Leaf
+-- 11 Inner
+-- 12 Leaf
+-- 13 Inner
+-- 14 Leaf
+-- 15 Root
