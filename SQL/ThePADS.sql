5b49dab90e7dabb6ac1e5ea8483133e4c6d151fd
Walter Guerrero <wsguerrero@gmail.com>
2021-06-11 15:42:22 -0600

    solved

--- SQL/ThePADS.sql
+++ SQL/ThePADS.sql
@@ -1,18 +1,18 @@
-Generate the following two result sets:
+-- Generate the following two result sets:
 
-1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+-- 1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
 
-2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+-- 2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
 
-There are a total of [occupation_count] [occupation]s.
+-- There are a total of [occupation_count] [occupation]s.
 
-where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+-- where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
 
-Note: There will be at least two entries in the table for each type of occupation.
+-- Note: There will be at least two entries in the table for each type of occupation.
 
-Input Format
+-- Input Format
 
-The OCCUPATIONS table is described as follows:
+-- The OCCUPATIONS table is described as follows:
 
 --      Column      |       Type
 -- --------------------------------
@@ -20,12 +20,12 @@ The OCCUPATIONS table is described as follows:
 --      Occupation  |     String
 
 
-Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+-- Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
 
 
-Sample Input
+-- Sample Input
 
-An OCCUPATIONS table that contains the following records:
+-- An OCCUPATIONS table that contains the following records:
 
 --  Name      |   Occupation 
 -- ---------------------------
@@ -41,24 +41,57 @@ An OCCUPATIONS table that contains the following records:
 --  Priya     |    Singer  
 
 
-Sample Output
+-- Sample Output
 
-Ashely(P)
-Christeen(P)
-Jane(A)
-Jenny(D)
-Julia(A)
-Ketty(P)
-Maria(A)
-Meera(S)
-Priya(S)
-Samantha(D)
-There are a total of 2 doctors.
-There are a total of 2 singers.
-There are a total of 3 actors.
-There are a total of 3 professors.
-Explanation
+-- Ashely(P)
+-- Christeen(P)
+-- Jane(A)
+-- Jenny(D)
+-- Julia(A)
+-- Ketty(P)
+-- Maria(A)
+-- Meera(S)
+-- Priya(S)
+-- Samantha(D)
+-- There are a total of 2 doctors.
+-- There are a total of 2 singers.
+-- There are a total of 3 actors.
+-- There are a total of 3 professors.
+-- Explanation
 
-The results of the first query are formatted to the problem description's specifications.
+-- The results of the first query are formatted to the problem description's specifications.
 
-The results of the second query are ascendingly ordered first by number of names corresponding to each profession (2 <= 2 <= 3 <=3), and then alphabetically by profession (doctor <= singer, and actor <= professor).
+-- The results of the second query are ascendingly ordered first by number of names corresponding to each profession (2 <= 2 <= 3 <=3), and then alphabetically by profession (doctor <= singer, and actor <= professor).
+
+
+-- CONCATE (https://www.w3schools.com/sql/func_mysql_concat.asp)
+-- LEFT https://www.w3schools.com/sql/func_mysql_left.asp
+
+SELECT CONCAT(NAME, '(', LEFT(Occupation, 1), ')') FROM OCCUPATIONS ORDER BY NAME ASC;
+
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.') FROM OCCUPATIONS GROUP BY Occupation ORDER BY COUNT(Occupation), Occupation;
+
+
+-- Your Output (stdout) / Expected Outcome
+-- Aamina(D)
+-- Ashley(P)
+-- Belvet(P)
+-- Britney(P)
+-- Christeen(S)
+-- Eve(A)
+-- Jane(S)
+-- Jennifer(A)
+-- Jenny(S)
+-- Julia(D)
+-- Ketty(A)
+-- Kristeen(S)
+-- Maria(P)
+-- Meera(P)
+-- Naomi(P)
+-- Priya(D)
+-- Priyanka(P)
+-- Samantha(A)
+-- There are a total of 3 doctors.
+-- There are a total of 4 actors.
+-- There are a total of 4 singers.
+-- There are a total of 7 professors.
