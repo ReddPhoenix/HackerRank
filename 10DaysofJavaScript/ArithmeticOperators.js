30041e5769574e7a89910c18f523585e9b3c1d37
Walter Guerrero <wsguerrero@gmail.com>
2021-07-07 22:14:40 -0600

    Objectives added

--- 10DaysofJavaScript/ArithmeticOperators.js
+++ 10DaysofJavaScript/ArithmeticOperators.js
@@ -1,63 +1,63 @@
-Objective
+// Objective
 
-In this challenge, we practice using arithmetic operators. Check out the attached tutorial for resources.
+// In this challenge, we practice using arithmetic operators. Check out the attached tutorial for resources.
 
-Task
+// Task
 
-Complete the following functions in the editor below:
+// Complete the following functions in the editor below:
 
-1. getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
-2. getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
+// 1. getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
+// 2. getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
 
-The values returned by these functions are printed to stdout by locked stub code in the editor.
-
-
-Input Format
-
-=============== getArea ==============
--------------------------------------
-DATA TYPE | PARAMETER |   DESCRIPTION
--------------------------------------
-Number    | length    | A number denoting the length of a rectangle
-Number    | height    | A number denoting the height of a rectangle
+// The values returned by these functions are printed to stdout by locked stub code in the editor.
 
 
-=== getPerimeter (length, height) ===
--------------------------------------
-DATA TYPE | PARAMETER |   DESCRIPTION
--------------------------------------
-Number    | length    | A number denoting the length of a rectangle
-Number    | height    | A number denoting the height of a rectangle
+// Input Format
+
+// =============== getArea ==============
+// -------------------------------------
+// DATA TYPE | PARAMETER |   DESCRIPTION
+// -------------------------------------
+// Number    | length    | A number denoting the length of a rectangle
+// Number    | height    | A number denoting the height of a rectangle
 
 
-Constraints
-
-* 1 <= length, width <= 1000
-* length and width are scaled to at most three decimal places.
-
-
-Output Format
-
--------------------------------------
-Function        | Return Type |   DESCRIPTION
--------------------------------------
-getArea         | Number      | The area of a rectangle having sides length and width
-getPerimeter    | Number      | The perimeter of a rectangle having sides length and width
+// === getPerimeter (length, height) ===
+// -------------------------------------
+// DATA TYPE | PARAMETER |   DESCRIPTION
+// -------------------------------------
+// Number    | length    | A number denoting the length of a rectangle
+// Number    | height    | A number denoting the height of a rectangle
 
 
-Sample Input 0
+// Constraints
 
-3
-4.5
-
-
-Sample Output 0
-
-13.5
-15
+// * 1 <= length, width <= 1000
+// * length and width are scaled to at most three decimal places.
 
 
-Explanation 0
+// Output Format
 
-The area of the rectangle is length x width = 3 x 4.5 = 13.5 .
-The perimeter of the rectangle is 2 x (length + width) = 2 x (3 + 4.5) = 15 .
+// -------------------------------------
+// Function        | Return Type |   DESCRIPTION
+// -------------------------------------
+// getArea         | Number      | The area of a rectangle having sides length and width
+// getPerimeter    | Number      | The perimeter of a rectangle having sides length and width
+
+
+// Sample Input 0
+
+// 3
+// 4.5
+
+
+// Sample Output 0
+
+// 13.5
+// 15
+
+
+// Explanation 0
+
+// The area of the rectangle is length x width = 3 x 4.5 = 13.5 .
+// The perimeter of the rectangle is 2 x (length + width) = 2 x (3 + 4.5) = 15 .
