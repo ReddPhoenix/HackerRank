28cad1083913b6ab9c11cebed1c7805e446603da
Walter Guerrero <wsguerrero@gmail.com>
2021-07-08 19:36:16 -0600

    day 1 complete

--- 10DaysofJavaScript/Functions.js
+++ 10DaysofJavaScript/Functions.js
@@ -33,3 +33,50 @@
 // Explanation 0
 
 // We return the value of 4! = 4 x 3 x 3 x 1 = 24 .
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+const factorial = (n) => {
+    if (n < 2) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+
+// Input (stdin)
+
+// Download
+// 4
+// Your Output (stdout)
+// 24
+// Expected Output
+
+// Download
+// 24
