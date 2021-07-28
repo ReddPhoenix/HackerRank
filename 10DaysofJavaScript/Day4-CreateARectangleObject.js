2b68f72f41fa3b0ad69fdbe61439bb59be845830
Walter Guerrero <wsguerrero@gmail.com>
2021-07-27 23:51:00 -0600

    variables assigned

--- 10DaysofJavaScript/Day4-CreateARectangleObject.js
+++ 10DaysofJavaScript/Day4-CreateARectangleObject.js
@@ -4,35 +4,41 @@
 
 // Task
 
-// Complete the function in the editor. It has two parameters:  and . It must return an object modeling a rectangle that has the following properties:
+// Complete the function in the editor. It has two parameters: a and b. It must return an object modeling a rectangle that has the following properties:
 
-// : This value is equal to .
-// : This value is equal to .
-// : This value is equal to 
-// : This value is equal to 
+// * length: This value is equal to a.
+// * width: This value is equal to b.
+// * perimeter: This value is equal to 2 x (a + b)
+// * area: This value is equal to a x b
 // Note: The names of the object's properties must be spelled correctly to pass this challenge.
 
 // Input Format
 
-// The first line contains an integer denoting .
-// The second line contains an integer denoting .
+// The first line contains an integer denoting a.
+// The second line contains an integer denoting b.
 
 // Constraints
+// * 1 < = a, b <= 100
+
 
 // Output Format
 
-// Return a object that has the properties specified above. Locked code in the editor prints the returned object's , , , and  to STDOUT.
+// Return a object that has the properties specified above. Locked code in the editor prints the returned object's length, width, perimeter, and area to STDOUT.
 
 // Sample Input 0
 
 // 4
 // 5
+
+
 // Sample Output 0
 
 // 4
 // 5
 // 18
 // 20
+
+
 // Explanation 0
 
-// Given a  of  and a  of , the Rectangle object's  is  and its  is .
+// Given a length of a=4 and a width  of b=5, the Rectangle object's perimeter is 4 + 4 + 5 + 5 = 18 and its area is 4 x 5 = 20.
