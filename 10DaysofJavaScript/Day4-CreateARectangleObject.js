4b3614965d73fce52a92d64ff5856990c30a974a
Walter Guerrero <wsguerrero@gmail.com>
2021-07-28 14:05:09 -0600

    day 4 complete

--- 10DaysofJavaScript/Day4-CreateARectangleObject.js
+++ 10DaysofJavaScript/Day4-CreateARectangleObject.js
@@ -42,3 +42,39 @@
 // Explanation 0
 
 // Given a length of a=4 and a width  of b=5, the Rectangle object's perimeter is 4 + 4 + 5 + 5 = 18 and its area is 4 x 5 = 20.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    this.length = a;
+    this.width = b;
+    this.perimeter = 2 * (a + b);
+    this.area = a * b;
+
+}
+
