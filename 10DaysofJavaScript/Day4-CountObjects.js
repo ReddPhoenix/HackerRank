fa4afc3a6933730f7bd72eed1593bfe8884f47d6
Walter Guerrero <wsguerrero@gmail.com>
2021-08-12 19:58:25 -0600

    day 4 complete

--- 10DaysofJavaScript/Day4-CountObjects.js
+++ 10DaysofJavaScript/Day4-CountObjects.js
@@ -50,3 +50,47 @@
 
 
 // Because we have two objects o that satisfy o.x == o.y (i.e., objects0 and objects2), we return 2 as our answer.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let count = 0;
+    
+    objects.forEach(o => {
+        if (o.x === o.y) {
+            count++;
+        }
+    })
+ 
+    return count;
+    
+}
+
